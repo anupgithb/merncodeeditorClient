ff1359fbb895823762610817b192e1ac80eacd56
anupgithb <anup.dutta@zimtive.com>
2023-07-16 01:14:10 +0530

    first commit 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'https://code-editor-api-z2u1.onrender.com/';
+const url = 'https://code-editor-api-z2u1.onrender.com';
 
 export const sendCode = async(codeWritten) =>{
     try {
