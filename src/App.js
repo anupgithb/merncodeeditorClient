ddbd0901e1ae9b5bc224213a6e670094303dc263
anupgithb <anup.dutta@zimtive.com>
2023-07-16 00:53:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Code from './components/Code.jsx';
+// import Ide from './components/Ide';
+import Editor from './components/Editor';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import DataProvider from './context/DataProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Editor />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
   );
 }
+
 
 export default App;
